5fdc3ae979a5da0e7e0578490fa2e81db3c7df34
zzl <2472351665@qq.com>
2025-01-09 10:37:01 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,14 +4,29 @@ const themeConfig = require('./config/themeConfig.js');
 
 module.exports = {
   theme: 'vdoing', // 使用npm包主题
-  title: "vdoing-template",
+  title: "迷雾星途",
   description: 'vdoing博客主题模板',
-  base: '/vdoing-template/', // 格式：'/<仓库名>/'， 默认'/'
+  base: '/迷雾星途/', // 格式：'/<仓库名>/'， 默认'/'
   markdown: {
     lineNumbers: true, // 代码行号
   },
 
   head,
-  plugins,
+  plugins: [
+    [
+      "vuepress-plugin-vssue-global",
+      {
+        platform: "github",
+        title: "[Comment]<%- frontmatter.title %>",
+        needComments: true,
+        // 其他的 Vssue 配置
+        autoCreateIssue: true,
+        clientId: "Ov23liOItqVHBjQs2Mlo",
+        clientSecret: "77cac56f580808132d91d5779a10d482614c0fb3",
+        owner: "Janzhou1020",
+        repo: "Janzhou1020.github.io",
+      },
+    ],
+  ],
   themeConfig,
 }
