c99652a0ecccd103f199bfd02c918ab7be07a016
zzl <2472351665@qq.com>
2025-01-11 11:21:25 +0800

    'commit'

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -3,8 +3,8 @@ module.exports = [
   { text: '首页', link: '/' },
   {
     text: '运维', link: '/oam', items: [
-      { text: '随手记录', link: '/three/' },
-      { text: '故障处理', link: '/three/' },
+      { text: '随手记录', link: '/hdd/' },
+      { text: '故障处理', link: '/fhd/' },
       { text: 'Nginx', link: '/nginx/' },
       { text: 'MySQL', link: '/mysql/' },
       { text: 'Ansible', link: '/ansible/' },
@@ -17,8 +17,8 @@ module.exports = [
   },
   {
     text: '编程', link: '/pge', items: [
-      { text: 'Shell', link: '/three/' },
-      { text: 'Python', link: '/three/' },
+      { text: 'Shell', link: '/shell/' },
+      { text: 'Python', link: '/python/' },
     ]
   },
   {
