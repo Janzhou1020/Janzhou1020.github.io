91d7aae004976417d18a73149cd17104ffa27129
zzl <2472351665@qq.com>
2025-01-13 15:19:17 +0800

    spider

--- docs/.vuepress/config/head.js
+++ docs/.vuepress/config/head.js
@@ -34,11 +34,5 @@ module.exports = [
       type: "text/javascript",
       src: "/js/pgmanor-self.js",
     },
-    "script",
-    {
-      language: "javascript",
-      type: "text/javascript",
-      src: "/js/spider.js",
-    },
   ],
 ];
